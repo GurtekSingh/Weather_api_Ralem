7f5123925351812ee04003ea53495adb50c87491
GurtekSingh <gurtekjattx@gmail.com>
2017-09-13 11:58:22 +0530

    new ui implement

---
+++ app/src/main/java/com/example/gurtek/weather_api_ralem/activites/WeatherDetailActivity.java
@@ -0,0 +1,10 @@
+package com.example.gurtek.weather_api_ralem.activites;
+
+/**
+ * * Created by Gurtek Singh on 9/12/2017.
+ * Sachtech Solution
+ * gurtekjattx@gmail.com .
+ */
+
+public class WeatherDetailActivity {
+}
