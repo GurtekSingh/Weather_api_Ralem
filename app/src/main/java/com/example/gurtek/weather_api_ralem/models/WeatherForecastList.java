29b3a132d3f78c43eab241c9ba4555a1da60e5d5
GurtekSingh <gurtekjattx@gmail.com>
2017-09-14 11:38:06 +0530

    forecasting units adding start

---
+++ app/src/main/java/com/example/gurtek/weather_api_ralem/models/WeatherForecastList.java
@@ -0,0 +1,10 @@
+package com.example.gurtek.weather_api_ralem.models;
+
+/**
+ * * Created by Gurtek Singh on 9/14/2017.
+ * Sachtech Solution
+ * gurtekjattx@gmail.com .
+ */
+
+public class WeatherForecastList {
+}
---
+++ app/src/test/java/com/example/gurtek/weather_api_ralem/presenter/ForeCastWeatherPresenterTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+
+/**
+ * * Created by Gurtek Singh on 9/14/2017.
+ * Sachtech Solution
+ * gurtekjattx@gmail.com .
+ */
+public class ForeCastWeatherPresenterTest {
+
+}
